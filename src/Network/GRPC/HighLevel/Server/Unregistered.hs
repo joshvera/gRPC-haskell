39a2798259d9ab4d6e09ba1fc299a1aa52fbe358
joshvera <josh@joshvera.com>
2018-05-17 20:05:45 -0400

    Bring back dispatchLoop and add asyncServerLoop and asyncDispatchLoop

--- src/Network/GRPC/HighLevel/Server/Unregistered.hs
+++ src/Network/GRPC/HighLevel/Server/Unregistered.hs
@@ -117,7 +117,52 @@ dispatchLoop :: Server
              -> [Handler 'ServerStreaming]
              -> [Handler 'BiDiStreaming]
              -> IO ()
-dispatchLoop s logger md hN hC hS hB = do
+dispatchLoop s logger md hN hC hS hB =
+  forever $ U.withServerCallAsync s $ \sc ->
+    case findHandler sc allHandlers of
+      Just (AnyHandler ah) -> case ah of
+        UnaryHandler _ h        -> unaryHandler sc h
+        ClientStreamHandler _ h -> csHandler sc h
+        ServerStreamHandler _ h -> ssHandler sc h
+        BiDiStreamHandler _ h   -> bdHandler sc h
+      Nothing                   -> unknownHandler sc
+  where
+    allHandlers = map AnyHandler hN ++ map AnyHandler hC
+                  ++ map AnyHandler hS ++ map AnyHandler hB
+
+    findHandler sc = find ((== U.callMethod sc) . anyHandlerMethodName)
+
+    unaryHandler :: (Message a, Message b) => U.ServerCall -> ServerHandler a b -> IO ()
+    unaryHandler sc h =
+      handleError $
+        U.serverHandleNormalCall' s sc md $ \_sc' bs ->
+          convertServerHandler h (const bs <$> U.convertCall sc)
+    csHandler :: (Message a, Message b) => U.ServerCall -> ServerReaderHandler a b -> IO ()
+    csHandler sc = handleError . U.serverReader s sc md . convertServerReaderHandler
+
+    ssHandler :: (Message a, Message b) => U.ServerCall -> ServerWriterHandler a b -> IO ()
+    ssHandler sc = handleError . U.serverWriter s sc md . convertServerWriterHandler
+
+    bdHandler :: (Message a, Message b) => U.ServerCall -> ServerRWHandler a b -> IO ()
+    bdHandler sc = handleError . U.serverRW s sc md . convertServerRWHandler
+
+    unknownHandler :: U.ServerCall -> IO ()
+    unknownHandler sc = void $ U.serverHandleNormalCall' s sc md $ \_ _ ->
+      return (mempty, mempty, StatusNotFound, StatusDetails "unknown method")
+
+    handleError :: IO a -> IO ()
+    handleError = (handleCallError logger . left herr =<<) . CE.try
+      where herr (e :: CE.SomeException) = GRPCIOHandlerException (show e)
+
+asyncDispatchLoop :: AsyncServer
+             -> (String -> IO ())
+             -> MetadataMap
+             -> [Handler 'Normal]
+             -> [Handler 'ClientStreaming]
+             -> [Handler 'ServerStreaming]
+             -> [Handler 'BiDiStreaming]
+             -> IO ()
+asyncDispatchLoop s logger md hN hC hS hB = do
   initialCallDataAsync <- processCallData s Listen Nothing hN-- C.grpcServerRequestCall >> returns (Process ServerCall)
   initialCallData <- wait initialCallDataAsync
   case initialCallData of
@@ -152,6 +197,37 @@ serverLoop ServerOptions{..} = do
   -- Without this fork, we block on a foreign call, which can't be interrupted.
   tid <- async $ withGRPC $ \grpc ->
     withServer grpc config $ \server -> do
+      dispatchLoop server
+                   optLogger
+                   optInitialMetadata
+                   optNormalHandlers
+                   optClientStreamHandlers
+                   optServerStreamHandlers
+                   optBiDiStreamHandlers
+  wait tid
+  where
+    config = ServerConfig
+      { host                             = optServerHost
+      , port                             = optServerPort
+      , methodsToRegisterNormal          = []
+      , methodsToRegisterClientStreaming = []
+      , methodsToRegisterServerStreaming = []
+      , methodsToRegisterBiDiStreaming   = []
+      , serverArgs                       =
+          [CompressionAlgArg GrpcCompressDeflate | optUseCompression]
+          ++
+          [ UserAgentPrefix optUserAgentPrefix
+          , UserAgentSuffix optUserAgentSuffix
+          ]
+      , sslConfig = optSSLConfig
+      }
+
+asyncServerLoop :: ServerOptions -> IO ()
+asyncServerLoop ServerOptions{..} = do
+  -- We run the loop in a new thread so that we can kill the serverLoop thread.
+  -- Without this fork, we block on a foreign call, which can't be interrupted.
+  tid <- async $ withGRPC $ \grpc ->
+    withAsyncServer grpc config $ \server -> do
       dispatchLoop server
                    optLogger
                    optInitialMetadata
